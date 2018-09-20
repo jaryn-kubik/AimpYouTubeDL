a78c884933bc16ebca27b7a176bf469a0af9197f
cubis12321 <cubis12321@gmail.com>
2018-09-20 17:51:45 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aimp_youtubedl
+{
+    public class Class1
+    {
+    }
+}
