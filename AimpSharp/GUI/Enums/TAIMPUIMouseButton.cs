1a935d308924072ba4577a24e244949476d5f21a
cubis12321 <cubis12321@gmail.com>
2020-07-12 13:32:06 +0200

    gui - enums

---
+++ AimpSharp/GUI/Enums/TAIMPUIMouseButton.cs
@@ -0,0 +1,9 @@
+﻿namespace AimpSharp.GUI.Enums
+{
+	public enum TAIMPUIMouseButton
+	{
+		Left = 0,
+		Right = 1,
+		Middle = 2
+	}
+}
---
+++ AimpSharp/GUI/Enums/TAIMPUITextAlignment.cs
@@ -0,0 +1,9 @@
+﻿namespace AimpSharp.GUI.Enums
+{
+	public enum TAIMPUITextAlignment
+	{
+		LeftJustify = 0,
+		RightJustify = 1,
+		Center = 2
+	}
+}
